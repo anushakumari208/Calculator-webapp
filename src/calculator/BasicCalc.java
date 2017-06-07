dd5eec588685f14bc3299178cf1b63dc8af74e1c
HOME <HOME@192.168.0.7>
2017-06-07 11:05:48 +0530

    Basic Calculator code for web application

--- src/calculator/BasicCalc.java
+++ src/calculator/BasicCalc.java
@@ -9,4 +9,29 @@ public class BasicCalc
 	
 	//include methods for operations
 	
+double a,b,c;
+public double geta(){
+	return a;}
+public void seta(double a){
+	this.a=a;}
+public double getb(){
+	return b;}
+public void setb(double b){
+	this.b=b;}
+
+double add(){
+c=a+b;
+return(c);}
+double subtract(){
+c=a-b;
+return(c);}
+double multiply(){
+c=a*b;
+return(c);}
+double divide(){
+c=a/b;
+return(c);}
+
 }
+
+
